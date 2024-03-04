f8d1dcd484a5e4719e7131da07fed3260770acf1
bunnynaughty12 <bunnynaughty12@gmail.com>
2024-03-04 12:07:14 +0700

    bh

--- obj/Debug/net8.0/WebBanHang.AssemblyInfo.cs
+++ obj/Debug/net8.0/WebBanHang.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebBanHang")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2d5a1249460c69be0c318d13bcb57a68d83b83c5")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebBanHang")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebBanHang")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
